597391c9c6e2c8c776720c1533a80a89a8ec9f2b
Mark Shields <laebshade@gmail.com>
2020-10-10 22:39:55 -0400

    remove unnecessary chown -R

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -6,8 +6,6 @@ mkdir -p /data/.session
 
 rm -f /data/.session/rtorrent.lock
 
-chown -R $UID:$GID /data /home/torrent /tmp /usr/flood /flood-db /etc/s6.d
-
 if [ ${RTORRENT_SOCK} = "false" ]; then
     sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:5000|' /home/torrent/.rtorrent.rc
 fi
